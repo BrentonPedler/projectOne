acd2b8040fd003be2d3ea95d8c6fc8b0cda66de1
BrentonPedler <brenton_pedler@hotmail.com>
2021-11-01 21:47:16 -0400

    adding alerts to inputs

---
+++ js/script.js
@@ -0,0 +1,43 @@
+// Adding Form Validation to blog form
+
+// Accessing form (DOM)
+
+const blogForm = document.getElementById('blogForm');
+
+
+
+// Create "submit" event
+
+blogForm.addEventListener('submit', function (e) {
+    
+    // Create Variable for NAME / EMAIL field (required)
+    
+    const name = document.getElementById('name');
+
+    const email = document.getElementById('email');
+
+    const nameData = name.value;
+
+    const emailData = email.value;
+
+    if (nameData == '') {
+
+        alert('Name Input Required');
+
+    }
+
+    if (emailData == '') {
+
+        alert('Email Input Required');
+
+    }
+
+
+
+
+});
+
+
+
+
+
